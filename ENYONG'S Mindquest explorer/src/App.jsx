7f28013542c178d6d87a78d2fd31796c61ae037a
john <139876338+0l3r7@users.noreply.github.com>
2025-11-22 19:30:34 +0800

    ahaha

--- ENYONG'S Mindquest explorer/src/App.jsx
+++ ENYONG'S Mindquest explorer/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       {/* Always render Home, even if modal is open */}
       <Routes location={state?.backgroundLocation || location}>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />      {/* Fallback */}
         <Route path="/setting" element={<Settings />} /> {/* Fallback */}
         <Route path="/account" element={<Account />} />
